30fd1ddad8080fd29a785245989794c55230f870
Bhesaniya Nirdesh Parsotambhai <nirdeshbhesaniya@gmail.com>
2025-04-18 15:36:41 +0530

    Black color is add in theme

--- src/dashboard/resume/components/ThemeColor.jsx
+++ src/dashboard/resume/components/ThemeColor.jsx
@@ -14,7 +14,7 @@ import { useSupabaseWithClerk } from '../../../../services/supabaseClient';
 function ThemeColor() {
   const colors = [
     '#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#A133FF',
-    '#33FFA1', '#FF7133', '#71FF33', '#7133FF', '#FF3371',
+    '#000', '#FF7133', '#71FF33', '#7133FF', '#FF3371',
     '#33FF71', '#3371FF', '#A1FF33', '#33A1FF', '#FF5733',
     '#5733FF', '#33FF5A', '#5A33FF', '#FF335A', '#335AFF'
   ];
